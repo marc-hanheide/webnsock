aba7eb8a80279b564de0f1bfd5433fed26237619
Marc Hanheide <marc@hanheide.net>
2017-07-30 07:24:17 +0100

    0.0.1

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 setup(
     name='webnsock',
     packages=['webnsock'],
-    package_data={'webnsock': ['www', 'www/*', 'www/*/*', 'www/*/*/*', 'www/*/*/*/*']},
-    version='0.4',
+    package_data={'webnsock': ['www/*', 'www/*/*', 'www/*/*/*', 'www/*/*/*/*']},
+    version='0.0.1',
     description='A web.py and websocket framework for interactive webservices',
     author='Marc Hanheide',
     author_email='marc@hanheide.net',
