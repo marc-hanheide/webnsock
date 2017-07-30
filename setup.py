19538117ea6fdec0205b9fb153b544ee1b3c992f
Marc Hanheide <marc@hanheide.net>
2017-07-30 07:17:15 +0100

    0.4

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
     name='webnsock',
     packages=['webnsock'],
     package_data={'webnsock': ['www', 'www/*', 'www/*/*', 'www/*/*/*', 'www/*/*/*/*']},
-    version='0.2',
+    version='0.4',
     description='A web.py and websocket framework for interactive webservices',
     author='Marc Hanheide',
     author_email='marc@hanheide.net',
     url='https://github.com/marc-hanheide/webnsock',  # use the URL to the github repo
-    download_url='https://github.com/marc-hanheide/webnsock/archive/0.2.tar.gz',  # I'll explain this in a second
+    download_url='https://github.com/marc-hanheide/webnsock/archive/0.4.tar.gz',  # I'll explain this in a second
     keywords=['web.py', 'websockets', 'webserver'],  # arbitrary keywords
     classifiers=[],
 )
