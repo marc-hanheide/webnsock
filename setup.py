8fbe44c634217f02b5f0b702cafbc3d693a0d8d0
Marc Hanheide <marc@hanheide.net>
2017-07-30 06:34:03 +0100

    first release try

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='webnsock',
+    packages=['webnsock'],
+    version='0.1',
+    description='A web.py and websocket framework for interactive webservices',
+    author='Marc Hanheide',
+    author_email='marc@hanheide.net',
+    url='https://github.com/marc-hanheide/webnsock',  # use the URL to the github repo
+    download_url='https://github.com/marc-hanheide/webnsock/archive/0.1.tar.gz',  # I'll explain this in a second
+    keywords=['web.py', 'websockets', 'webserver'],  # arbitrary keywords
+    classifiers=[],
+)
